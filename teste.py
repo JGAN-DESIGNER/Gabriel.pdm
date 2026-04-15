c7273b0644ff405f3f3c5cc0b3e48eae4dea3c29
João Gabriel Alves Nunes <nunes.alves@academico.ifpb.edu.br>
2026-04-15 11:01:20 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.image("fotogabriel.jpeg")
 st.write("Gabriel Alves")
-st.link_button("Acessar", "https://www.instagram.com/jgan.designer/")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/gabrielalves51/in%C3%ADcio")
